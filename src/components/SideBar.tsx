2bf6716baffd6b972414128a5caedafa03ba6ca3
Feglawy <a.m9518520963@outlook.com>
2024-08-13 02:39:34 +0300

    fixed sidebar active class

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,21 @@
 import ThemeToggler from "./ThemeToggler";
 import "../styles/SideBar.css";
+import { useLocation } from "react-router-dom";
 
 const SideBar = () => {
+	const location = useLocation();
+	const isActive = (path: string) => location.pathname === path;
+
 	return (
 		<div className="sidebar">
 			<a className="logo" href="/"></a>
 			<div className="links">
-				<a href="/" className="active">
+				<a href="/" className={isActive("/") ? "active" : ""}>
 					Accounts
 				</a>
-				<a href="/add">Add</a>
+				<a href="/add" className={isActive("/add") ? "active" : ""}>
+					Add
+				</a>
 			</div>
 			<ThemeToggler />
 		</div>
